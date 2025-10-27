d75fd5b43dd38756e91d07cfda8907d5ed1b7893
OweQian <1510106069@qq.com>
2025-10-27 17:32:00 +0800

    feat: if语句

--- toppings.py
+++ toppings.py
@@ -22,4 +22,12 @@ if requested_toppings:
         print(f"Adding {requested_topping}.")
     print("\nFinished making your pizza!")
 else:
-    print("Are you sure you want a plain pizza?")                     
+    print("Are you sure you want a plain pizza?")             
+    
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']    
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!")            
