88f750b328714ec7160fa8e4ffe657900c9e8e8a
OweQian <1510106069@qq.com>
2025-11-27 14:46:49 +0800

    feat: python标准库

---
+++ choice.py
@@ -0,0 +1,4 @@
+from random import choice
+players = ['charlie', 'alice', 'bob', 'david']
+first_up = choice(players)
+print(first_up)
---
+++ randint.py
@@ -0,0 +1,3 @@
+from random import randint
+random = randint(1, 6)
+print(random)
