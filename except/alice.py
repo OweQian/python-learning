a425bbe74674ebdc0980f14c5dd50b009d748d69
OweQian <1510106069@qq.com>
2025-12-02 14:18:35 +0800

    feat: 异常

---
+++ except/alice.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def count_words(path):
+  """计算一个文件大致包含多少个单词"""
+  try:
+    contents = path.read_text(encoding='utf-8')
+  except FileNotFoundError:
+    pass
+  else:
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
+    
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+  path = Path(filename)
+  count_words(path)
